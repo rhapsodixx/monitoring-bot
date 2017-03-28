b0360e1df66736b732c8ac87e02552e676612932
panji gautama <panji.gautama@gmail.com>
2017-03-28 20:31:03 +0700

    Add webhook functionality for bot

--- routes/bots.js
+++ routes/bots.js
@@ -1,30 +1,13 @@
 var express = require('express');
 var TelegramBot = require('node-telegram-bot-api');
+var axios = require('axios');
+
 var router = express.Router();
-var token = '307103339:AAHYmSkL-fF10kZC7va99dzOMVU5grASXwc';
+var telegramToken = '307103339:AAHYmSkL-fF10kZC7va99dzOMVU5grASXwc';
 
-// Setup polling way
-var bot = new TelegramBot(token, { polling: true });
+// Setup telegram bot library
+var bot = new TelegramBot(token, { polling: false });
 console.log('bot server started...');
-
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
-  var fromId = msg.from.id;
-  var resp = match[1];
-  bot.sendMessage(fromId, resp);
-});
-
-bot.onText(/\/status/, function (msg, match) {
-  var fromId = msg.from.id;
-  var insults = ["Dumbass", "Out of 100,000 sperm, you were the fastest?", "Look, you aint funny. Your life is just a joke."];
-  var chosenInsult = insults[Math.floor(Math.random() * insults.length)];
-  bot.sendMessage(fromId, chosenInsult);
-});
-
-bot.onText(/\/help/, function (msg, match) {
-  var fromId = msg.from.id;
-  bot.sendMessage(fromId, "This spectacular bot just have one single command.\n/insult - Insult you.");
-});
 
 // Any kind of message
 bot.on('message', function (msg, match) {
@@ -41,6 +24,31 @@ router.get('/', function (req, res, next) {
 // webhook
 router.get('/webhook', function (req, res, next) {
   res.render('index', { title: 'Monitoring Bot Webhook' });
+});
+
+router.post('/webhook', function (req, res, next) {
+  var message = req.body;
+
+  // Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+  if (!message || message.text.toLowerCase().indexOf('help') < 0) {
+    // In case a message is not present, or if our message does not have the word help in it, do nothing and return an empty response
+    return res.end()
+  }
+
+  console.log('send telegram');
+  axios.post('https://api.telegram.org/bot' + telegramToken + '/sendMessage', {
+    chat_id: message.chat.id,
+    text: 'Polo!!'
+  }).then(function (response) {
+    // We get here if the message was successfully posted
+    console.log('Message posted');
+    res.end('ok');
+  }.catch(function (err) {
+    // ...and here if it was not
+    console.log('Error :', err);
+    res.end('Error :' + err);
+  }));
+
 });
 
 module.exports = router;
