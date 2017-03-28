a0a88e2f22320680360c653d240c1eb05143c00f
panji gautama <panji.gautama@gmail.com>
2017-03-28 20:18:24 +0700

    Change api token for telegram bot

--- routes/bots.js
+++ routes/bots.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var TelegramBot = require('node-telegram-bot-api');
 var router = express.Router();
-var token = '312620597:AAEDD_qnU7xgk6pzkJjyue6Ooehi7cLBF6k';
+var token = '307103339:AAHYmSkL-fF10kZC7va99dzOMVU5grASXwc';
 
 // Setup polling way
 var bot = new TelegramBot(token, { polling: true });
@@ -14,14 +14,14 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
   bot.sendMessage(fromId, resp);
 });
 
-bot.onText(/\/status/, function(msg, match) {
+bot.onText(/\/status/, function (msg, match) {
   var fromId = msg.from.id;
   var insults = ["Dumbass", "Out of 100,000 sperm, you were the fastest?", "Look, you aint funny. Your life is just a joke."];
   var chosenInsult = insults[Math.floor(Math.random() * insults.length)];
   bot.sendMessage(fromId, chosenInsult);
 });
 
-bot.onText(/\/help/, function(msg, match) {
+bot.onText(/\/help/, function (msg, match) {
   var fromId = msg.from.id;
   bot.sendMessage(fromId, "This spectacular bot just have one single command.\n/insult - Insult you.");
 });
@@ -31,6 +31,16 @@ bot.on('message', function (msg, match) {
   var fromId = msg.from.id;
   var resp = match[1];
   bot.sendMessage(fromId, resp);
+});
+
+// bots index
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Monitoring Bot Index' });
+});
+
+// webhook
+router.get('/webhook', function (req, res, next) {
+  res.render('index', { title: 'Monitoring Bot Webhook' });
 });
 
 module.exports = router;
